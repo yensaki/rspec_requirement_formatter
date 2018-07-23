6469f8dc217857bbe8228fa85c39c0eed1f09659
yensaki <mov.an.double@gmail.com>
2018-07-24 01:31:17 +0900

    Load rspec, first

--- lib/rspec_requirement_formatter.rb
+++ lib/rspec_requirement_formatter.rb
@@ -1,5 +1,8 @@
 require "rspec_requirement_formatter/version"
 
+require "rspec"
+require "rspec/core/formatters/base_formatter"
+
 require "rspec_requirement_formatter/html_formatter"
 
 module RspecRequirementFormatter
