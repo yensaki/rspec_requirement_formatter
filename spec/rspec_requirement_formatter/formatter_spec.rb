d68441f0a470f2d27827a5b466ee95cf116abb8f
yensaki <mov.an.double@gmail.com>
2020-08-30 19:00:12 +0900

    Adjust RSpec contexts

--- spec/rspec_requirement_formatter/formatter_spec.rb
+++ spec/rspec_requirement_formatter/formatter_spec.rb
@@ -8,20 +8,22 @@ require 'rspec_requirement_formatter'
 RSpec.describe RspecRequirementFormatter::Formatter do
   include FormatterSupport
 
-  describe "produced JSON" do
+  describe "output" do
     let(:expected_file_path) { File.expand_path('../../fixtures/expected.json', __FILE__) }
     let(:expected_file) { File.read(expected_file_path) }
 
-    it 'generate JSON' do
-      its = []
-      group = RSpec.describe("one apiece") do
-        its.push it("succeeds") { expect(1).to eq 1 }
+    context "When simple RSpec group," do
+      it "generates formatted JSON" do
+        its = []
+        group = RSpec.describe("one apiece") do
+          its.push it("succeeds") { expect(1).to eq 1 }
+        end
+        reporter.report(2) do |r|
+          group.run(r)
+        end
+        # run_rspec(described_class.to_s, output: output_path, spec_path: [example_spec_file_path])
+        expect(formatter_output.string).to eq expected_file
       end
-      reporter.report(2) do |r|
-        group.run(r)
-      end
-      # run_rspec(described_class.to_s, output: output_path, spec_path: [example_spec_file_path])
-      expect(formatter_output.string).to eq expected_file
     end
   end
 end
