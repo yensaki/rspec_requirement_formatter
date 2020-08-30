4e6e4449bdbfaedb392ba3431083a4c7b4d58767
yensaki <mov.an.double@gmail.com>
2020-08-30 21:27:48 +0900

    Test: nested example_groups

--- spec/rspec_requirement_formatter/formatter_spec.rb
+++ spec/rspec_requirement_formatter/formatter_spec.rb
@@ -9,19 +9,46 @@ RSpec.describe RspecRequirementFormatter::Formatter do
   include FormatterSupport
 
   describe "output" do
-    let(:expected_file_path) { File.expand_path('../../fixtures/expected.json', __FILE__) }
-    let(:expected_file) { File.read(expected_file_path) }
-
     context "When simple RSpec group," do
+      let!(:expected_file_path) { File.expand_path('../../fixtures/expected/simple_example_group.json', __FILE__) }
+      let!(:expected_file) { File.read(expected_file_path) }
+
       it "generates formatted JSON" do
-        its = []
         group = RSpec.describe("one apiece") do
-          its.push it("succeeds") { expect(1).to eq 1 }
+          it("succeeds") { expect(1).to eq 1 }
         end
         reporter.report(2) do |r|
           group.run(r)
         end
         # run_rspec(described_class.to_s, output: output_path, spec_path: [example_spec_file_path])
+        expect(formatter_output.string).to eq expected_file
+      end
+    end
+
+    context "When nested RSpec groups," do
+      let!(:expected_file_path) { File.expand_path('../../fixtures/expected/nested_example_groups.json', __FILE__) }
+      let!(:expected_file) { File.read(expected_file_path) }
+
+      it "generates formatted JSON" do
+        group = RSpec.describe("one apiece") do
+          it("succeeds") { expect(1).to eq 1 }
+
+          context "context1" do
+            context "context1-1" do
+              it("context1-1_example1") { expect(1).to eq 1 }
+            end
+
+            context "context1-2" do
+              it("context1-2_example1") { expect(1).to eq 1 }
+              it("context1-2_example2") { expect(1).to eq 1 }
+            end
+
+            it("context1_example1") { expect(1).to eq 1 }
+          end
+        end
+        reporter.report(2) do |r|
+          group.run(r)
+        end
         expect(formatter_output.string).to eq expected_file
       end
     end
