ce1318bd86aff482c8659b0c83f48812e8318c63
yensaki <mov.an.double@gmail.com>
2020-08-30 12:00:15 +0900

    Split to Formatter

--- lib/rspec_requirement_formatter.rb
+++ lib/rspec_requirement_formatter.rb
@@ -1,40 +1,5 @@
 require "rspec_requirement_formatter/version"
+require "rspec_requirement_formatter/formatter"
 
-require "rspec"
-require "rspec/core/formatters/base_formatter"
-
-class RspecRequirementFormatter < RSpec::Core::Formatters::BaseFormatter
-  RSpec::Core::Formatters.register self,
-                                   :example_group_started,
-                                   :example_started,
-                                   :example_passed,
-                                   :example_failed,
-                                   :example_pending,
-                                   :example_group_finished
-
-  def initialize(output)
-    super(output)
-  end
-
-  def example_group_started(notification)
-    super
-  end
-
-  def example_started(notification)
-  end
-
-  def example_passed(passed)
-  end
-
-  def example_failed(failed)
-  end
-
-  def example_pending(pending)
-  end
-
-  def example_group_finished(notification)
-  end
-
-  def close(notification)
-  end
+module RspecRequirementFormatter
 end
